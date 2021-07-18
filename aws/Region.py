1233e638de23e03c44b0d516f0dfffeac9a9e497
supermuesli <art.lal@hotmail.com>
2021-07-19 00:44:48 +0200

    update matchCCS

--- aws/Region.py
+++ aws/Region.py
@@ -5,7 +5,7 @@ class Region(Attribute):
     def __init__(self):
         super().__init__()
         self.setId("https://github.com/supermuesli/csdl", "aws/Region.py")
-        self.extendsId = "ChoiceAttribute"
+        self.extendsId = "Region"
 
         self.options = ["East Virginia", "New York", "California"]
         self.value = self.options[0]
