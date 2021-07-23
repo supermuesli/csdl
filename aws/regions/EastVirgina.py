11473334492a976960f7577a2f969fbb53fc14e1
supermuesli <art.lal@hotmail.com>
2021-07-23 09:21:39 +0200

    draft

--- aws/regions/EastVirgina.py
+++ aws/regions/EastVirgina.py
@@ -4,7 +4,7 @@ from csdl import *
 class EastVirgina(Attribute):
     def __init__(self):
         super().__init__()
-        self.setId("https://github.com/supermuesli/csdl", "aws/regions/EastVirgina.py")
+        self.setId("https://github.com/supermuesli/csdl", "aws/regions/EastVirginia.py")
         self.extendsId = "https://github.com/supermuesli/csdl@misc/countries/USA.py@latest"
 
         self.value = "East Virgina"
