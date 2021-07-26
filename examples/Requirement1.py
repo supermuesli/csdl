3c3a9e040edd0a458153a00757ad8dbc5bcb9696
supermuesli <art.lal@hotmail.com>
2021-07-26 06:56:34 +0200

    draft

--- examples/Requirement1.py
+++ examples/Requirement1.py
@@ -5,7 +5,7 @@ class Requirement1(CCS):
     def __init__(self):
         super().__init__()
         self.setId("https://github.com/supermuesli/csdl", "examples/Requirement1.py")
-        self.extendsId = "VMAsAService"
+        self.extendsId = "StorageAsAService"
 
         self.ram.value = 4
         self.cpuCores.value = 4
