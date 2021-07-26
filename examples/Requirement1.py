f42e6e2cf6816c35573c97a483ac3a46e42cc261
supermuesli <art.lal@hotmail.com>
2021-07-26 06:53:14 +0200

    draft

--- examples/Requirement1.py
+++ examples/Requirement1.py
@@ -15,6 +15,10 @@ class Requirement1(CCS):
         self.staticIpAddresses.inject("https://github.com/supermuesli/csdl", "misc/StaticIp.py")
         self.staticIpAddresses.value = 200
 
+        usa = NameAttribute()
+        usa.inject("https://github.com/supermuesli/csdl", "misc/countries/USA.py")
+        self.region.options = {"USA": usa}
         self.region.choice = "USA"
+        
         self.price.model.choice = "subscription"  # pay per resource
         self.price.currency = "EUR"
