98cb3c416170d7c22a2a88074e99c832e939aa70
supermuesli <art.lal@hotmail.com>
2021-10-05 19:40:11 +0200

    added aws s3 ccs and its attributes

--- examples/StorageRequirement.py
+++ examples/StorageRequirement.py
@@ -25,3 +25,27 @@ class StorageRequirement(Attribute):
         self.s3type = Attribute()
         self.s3type.inject("https://github.com/supermuesli/csdl", "aws/s3/S3Type.py")
         self.s3type.value = "standard"
+
+        self.putAmount = Attribute()
+        self.putAmount.inject("https://github.com/supermuesli/csdl", "misc/requests/PUTAmount.py")
+        self.putAmount.value = 600
+
+        self.copyAmount = Attribute()
+        self.copyAmount.inject("https://github.com/supermuesli/csdl", "misc/requests/COPYAmount.py")
+        self.copyAmount.value = 202
+
+        self.listAmount = Attribute()
+        self.listAmount.inject("https://github.com/supermuesli/csdl", "misc/requests/LISTAmount.py")
+        self.listAmount.value = 900
+
+        self.getAmount = Attribute()
+        self.getAmount.inject("https://github.com/supermuesli/csdl", "misc/requests/GETAmount.py")
+        self.getAmount.value = 13000
+
+        self.selectAmount = Attribute()
+        self.selectAmount.inject("https://github.com/supermuesli/csdl", "misc/requests/SELECTAmount.py")
+        self.selectAmount.value = 42
+
+        self.postAmount = Attribute()
+        self.postAmount.inject("https://github.com/supermuesli/csdl", "misc/requests/POSTAmount.py")
+        self.postAmount.value = 10420
