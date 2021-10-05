9513679919a88136c331adb4138a30241d2fbe3e
supermuesli <art.lal@hotmail.com>
2021-10-05 15:36:37 +0200

    added aws s3 ccs and its attributes

--- examples/StorageRequirement.py
+++ examples/StorageRequirement.py
@@ -10,5 +10,5 @@ class StorageRequirement(Attribute):
         self.storage.value = 5000
 
         self.region.inject("https://github.com/supermuesli/csdl", "aws/regions/Region.py")
-        self.region.value = "northernVirignia"
+        self.region.value = "northernVirginia"
 
