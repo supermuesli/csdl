c0a824c8a0849904e1cda63c5466eca016bc44d5
supermuesli <art.lal@hotmail.com>
2021-10-13 00:49:26 +0200

    bump

--- aws/rdsPostgres/Postgresql.py
+++ aws/rdsPostgres/Postgresql.py
@@ -274,13 +274,14 @@ class Postgresql(CCS):
                                 if transferOutMatch.value is not None:
                                     if transferOutMatch.value <= 1:
                                         res += 0.00 * transferOutMatch.value
-                                    elif 1 < transferOutMatch.value <= 9.99*1000:
+                                    if 1 < transferOutMatch.value <= 1 + 9.999 * 1000:
                                         res += 0.09 * transferOutMatch.value
-                                    elif 9.99*1000 < transferOutMatch.value <= 40*1000:
+                                    elif 1 + 9.999 * 1000 < transferOutMatch.value <= 1 + (9.999 + 40) * 1000:
                                         res += 0.085 * transferOutMatch.value
-                                    elif 40*1000 < transferOutMatch.value <= 150*1000:
+                                    elif 1 + (9.999 + 40) * 1000 < transferOutMatch.value <= 1 + (
+                                            9.999 + 40 + 100) * 1000:
                                         res += 0.07 * transferOutMatch.value
-                                    elif 150*1000 < transferOutMatch.value:
+                                    elif 1 + (9.999 + 40 + 100) * 1000 < transferOutMatch.value:
                                         res += 0.05 * transferOutMatch.value
 
                 return res
